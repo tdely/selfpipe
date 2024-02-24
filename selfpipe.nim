2a927893b8a4c0f0a996e2e4c3e26e0d8d2ade9b
Tobias Dély <cleverhatcamouflage@gmail.com>
2024-02-24 12:59:21 +0100

    docs: add documentation comments

--- selfpipe.nim
+++ selfpipe.nim
@@ -4,7 +4,10 @@ const bsize: cint = 2 # selfpipe read buffer size
 
 type
   SigProc* = proc() {.gcsafe.}
+    ## Handler for signal.
   SigSet* = TableRef[cint, SigProc]
+    ## Set of signals for the selfpipe to handle and the `SigProc` that trigger
+    ## on respective signal.
 
 var pipefds = cast[ptr array[2, cint]](allocShared0(sizeof(array[2, cint])))
 
@@ -30,11 +33,13 @@ when compileOption("threads") and defined(selfpipe_thread):
       elif r == 0: break
 
   proc registerLogger*(logger: Logger) =
+    ## Register logger to be used in the signal monitoring thread.
     loggers[].add(logger)
 else:
   var pollfd: TPollfd
 
   proc checkSignal*(sigSet: SigSet) =
+    ## Check if signal has been received and execute corresponding `SigProc`.
     var buf: array[bsize, char]
     if poll(addr pollfd, 1, 0) > 0:
       if pollfd.revents == POLLIN:
@@ -49,16 +54,23 @@ else:
             sigSet[sig]()
 
 proc newSigSet*(): SigSet =
+  ## Create a new `SigSet`.
   newTable[cint, SigProc]()
 
 proc add*(sigSet: SigSet, sig: cint, p: SigProc) =
+  ## Add `sig` mapped to `p` to `sigSet`.
   sigSet[sig] = p
 
 proc sendSignal*(sig: cint) =
+  ## Send signal internally. Does not trigger the signal handler.
   if pipefds[][0] == 0: raise newException(Defect, "sendSignal called before init")
   discard write(pipefds[][1], cstring($sig), cint(len($sig)))
 
 proc init*(sigSet: sink SigSet): cint =
+  ## Initialize the selfpipe using `sigSet`.
+  ##
+  ## Returns 0 on success, else `errno` from the operation that failed (pipe or
+  ## fcntl).
   if pipefds[][0] != 0: raise newException(Defect, "init called after init")
   if pipe(pipefds[]) == -1:
     return posix.errno
@@ -74,6 +86,7 @@ proc init*(sigSet: sink SigSet): cint =
       discard write(pipefds[][1], cstring($sig), cint(len($sig)))
 
 proc finish*() =
+  ## Close selfpipe resources.
   # helgrind reports these close() calls as possible data races
   discard close(pipefds[0])
   discard close(pipefds[1])
