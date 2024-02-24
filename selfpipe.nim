eee0f208d411b5d63f83bed08ea78d9603323e9a
Tobias Dély <cleverhatcamouflage@gmail.com>
2024-02-24 23:04:44 +0100

    feat: remove thread code
    Let the user implement the threading if wanted.

--- selfpipe.nim
+++ selfpipe.nim
@@ -11,17 +11,13 @@ type
 
 var pipefds = cast[ptr array[2, cint]](allocShared0(sizeof(array[2, cint])))
 
-when compileOption("threads") and defined(selfpipe_thread):
-  import std/logging
-  var
-    thr: Thread[SigSet]
-    loggers = cast[ptr seq[Logger]](allocShared0(sizeof(seq[Logger])))
+var pollfd: TPollfd
 
-  proc monitorSignals(sigSet: SigSet) {.thread.} =
-    for logger in loggers[]:
-      addHandler(logger)
-    while true:
-      var buf: array[bsize, char]
+proc checkSignal*(sigSet: SigSet) =
+  ## Check if signal has been received and execute corresponding `SigProc`.
+  var buf: array[bsize, char]
+  if poll(addr pollfd, 1, 0) > 0:
+    if pollfd.revents == POLLIN:
       let r = read(pipefds[][0], addr buf, bsize)
       if r > 0:
         var s: string
@@ -32,30 +28,6 @@ when compileOption("threads") and defined(selfpipe_thread):
         let sig = cint(parseInt(s))
         if sigSet.hasKey(sig):
           sigSet[sig]()
-      elif r == 0:
-        break
-
-  proc registerLogger*(logger: Logger) =
-    ## Register logger to be used in the signal monitoring thread.
-    loggers[].add(logger)
-else:
-  var pollfd: TPollfd
-
-  proc checkSignal*(sigSet: SigSet) =
-    ## Check if signal has been received and execute corresponding `SigProc`.
-    var buf: array[bsize, char]
-    if poll(addr pollfd, 1, 0) > 0:
-      if pollfd.revents == POLLIN:
-        let r = read(pipefds[][0], addr buf, bsize)
-        if r > 0:
-          var s: string
-          for x in buf:
-            if x == '\x00':
-              break
-            s.add x
-          let sig = cint(parseInt(s))
-          if sigSet.hasKey(sig):
-            sigSet[sig]()
 
 proc newSigSet*(): SigSet =
   ## Create a new `SigSet`.
@@ -83,18 +55,12 @@ proc init*(sigSet: sink SigSet): cint =
   if fcntl(pipefds[][0], F_SETFD, O_NONBLOCK) == -1 or
       fcntl(pipefds[][1], F_SETFD, O_NONBLOCK) == -1:
     return posix.errno
-  when compileOption("threads") and defined(selfpipe_thread):
-    createThread(thr, monitorSignals, sigSet)
-  else:
-    pollfd = TPollfd(fd: pipefds[][0], events: POLLIN, revents: 0)
+  pollfd = TPollfd(fd: pipefds[][0], events: POLLIN, revents: 0)
   for sig, _ in sigset:
     onsignal(sig):
       discard write(pipefds[][1], cstring($sig), cint(len($sig)))
 
 proc finish*() =
   ## Close selfpipe resources.
-  # helgrind reports these close() calls as possible data races
   discard close(pipefds[0])
   discard close(pipefds[1])
-  when compileOption("threads") and defined(selfpipe_thread):
-    thr.joinThread()
